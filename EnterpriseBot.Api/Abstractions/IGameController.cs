0fa6a9d97a5f0977d3ef85d9227fe17f805e2719
Deeplerg <47917846+Deeplerg@users.noreply.github.com>
2020-07-06 19:29:32 +0500

    Tiny IGameController rework

--- EnterpriseBot.Api/Abstractions/IGameController.cs
+++ EnterpriseBot.Api/Abstractions/IGameController.cs
@@ -3,13 +3,21 @@ using System.Threading.Tasks;
 
 namespace EnterpriseBot.Api.Abstractions
 {
-    public interface IGameController<TModel> where TModel : class
+    public interface IGameController<TModel, TCreationParams> where TModel : class
+                                                              where TCreationParams : class
     {
         /// <summary>
-        /// Returns an object with specified <paramref name="idpar"/>
+        /// Returns <typeparamref name="TModel"/> with specified <paramref name="idpar"/>
         /// </summary>
-        /// <param name="idpar">Id of the object</param>
-        /// <returns>Object with specified <paramref name="idpar"/></returns>
+        /// <param name="idpar">Id of <typeparamref name="TModel"/></param>
+        /// <returns><typeparamref name="TModel"/> with specified <paramref name="idpar"/></returns>
         Task<GameResult<TModel>> Get(IdParameter idpar);
+
+        /// <summary>
+        /// Creates <typeparamref name="TModel"/> from <typeparamref name="TCreationParams"/> and returns it back
+        /// </summary>
+        /// <param name="pars">Parameters from which the <typeparamref name="TModel"/> will be created</param>
+        /// <returns>Created <typeparamref name="TModel"/></returns>
+        Task<GameResult<TModel>> Create(TCreationParams pars);
     }
 }

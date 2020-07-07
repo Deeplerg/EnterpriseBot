29d92e24b943b941d34d29968236369c825b1a5f
Deeplerg <47917846+Deeplerg@users.noreply.github.com>
2020-07-07 15:32:05 +0500

    GameSettings fix names

--- EnterpriseBot.Api/Models/Settings/GameSettings.cs
+++ EnterpriseBot.Api/Models/Settings/GameSettings.cs
@@ -11,7 +11,7 @@ namespace EnterpriseBot.Api.Models.Settings
         public BusinessSettings.BusinessSettings Business { get; set; }
         public DonationSettings.DonationSettings Donation { get; set; }
         public GameplaySettings.GameplaySettings Gameplay { get; set; }
-        public LocalizationSettings.LocalizationSettings LocalizationSettings { get; set; }
-        public MarketSettings.MarketSettings MarketSettings { get; set; }
+        public LocalizationSettings.LocalizationSettings Localization { get; set; }
+        public MarketSettings.MarketSettings Market { get; set; }
     }
 }
